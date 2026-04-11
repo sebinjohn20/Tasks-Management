b734b41b664edccca9a69ec0d80876a83cfd4ea6
unknown <sebinjohn20@gmail.com>
2026-04-11 11:05:53 +0530

    code

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,21 +1,25 @@
+export const dynamic = "force-dynamic";
+
 import TaskOverView from "@/components/task-overview";
 
-async function fecthListOfTasks() {
+async function fetchListOfTasks() {
   try {
     const apiResponse = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/get-blogs`,
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/get-tasks`,
       {
         method: "GET",
         cache: "no-store",
       },
     );
+
     const result = await apiResponse.json();
     return result?.data;
   } catch (error) {
     throw new Error(error);
   }
 }
+
 export default async function Tasks() {
-  const taskList = await fecthListOfTasks();
+  const taskList = await fetchListOfTasks();
   return <TaskOverView taskList={taskList} />;
 }
